89ce77aaee33f780e29cc970738cd37e87ac4e47
Quinten Krauth <quintenkrauth19@gmail.com>
2023-12-17 15:55:37 -0500

    trying to start server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+// require("dotenv").config();
+
+// const express = require("express");
+// const cors = require("cors");
+
+// const app = express();
+
+// const {PORT} = process.env;
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.listen(PORT, () => console.log(`server running on port ${PORT}`));
