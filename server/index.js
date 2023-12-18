31341261cef8ea3fa8fddf3e663c94676be48d50
Quinten Krauth <quintenkrauth19@gmail.com>
2023-12-18 17:22:22 -0500

    backend server running on seperate port not sure if ideal

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
-// require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
-// const express = require("express");
-// const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-// const app = express();
 
-// const {PORT} = process.env;
+app.use(express.json());
+app.use(cors());
 
-// app.use(express.json());
-// app.use(cors());
-
-// app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
