9116633c5189dca7a68773f088f9e5b741fb11b7
Quinten Krauth <quintenkrauth19@gmail.com>
2024-01-10 12:59:17 -0500

    intro changes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,7 +8,7 @@ const Intro = () => {
             <div className="intro-para">
                 <h6>Welcome to DevTrail, where we believe every journey into tech is a unique adventure waiting to unfold. At DevTrail,
                 we're more than a startup – we're your compass, guiding you through the trails of technology, transforming dreams into thriving careers.
-                <span>Your new career adventure begins here at DevTrail</span> – Trailblazing Your Tech Future!</h6>
+                <span> Your new career adventure begins here at DevTrail</span> – Trailblazing Your Tech Future!</h6>
             </div>
             <button className="intro-button">Learn More</button>
         </div>
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -7,8 +7,8 @@ const HomeScreen = () => {
             <div className="home-section-1">
                     <Intro />
                     <div className="pics-container">
-                    <img src="https://www.jcpenneyoptical.com/wp-content/uploads/2018/04/ANA_AN176-O-90-52-18-135_profil_F1_M8_XL.png" alt="" />
-                    <img src="https://i.dell.com/is/image/DellContent/content/dam/images/products/electronics-and-accessories/dell/keyboards/km5221w-wth/km5221w-ckb-06-wh.psd?     fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=713&qlt=100,1&resMode=sharp2&size=713,402&chrss=full" alt="" />
+                    <img className="img1" src="https://www.jcpenneyoptical.com/wp-content/uploads/2018/04/ANA_AN176-O-90-52-18-135_profil_F1_M8_XL.png" alt="" />
+                    <img className="img2" src="https://i.dell.com/is/image/DellContent/content/dam/images/products/electronics-and-accessories/dell/keyboards/km5221w-wth/km5221w-ckb-06-wh.psd?     fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=713&qlt=100,1&resMode=sharp2&size=713,402&chrss=full" alt="" />
                     </div>
             </div>
         </div>
