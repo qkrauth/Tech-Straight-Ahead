387392bd9096e07e31ec2a9d6cf3e1ce8e10788d
Quinten Krauth <quintenkrauth19@gmail.com>
2024-04-19 13:48:50 -0400

    frontend routes start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
-import Header from './components/Header';
-import HomeScreen from './screens/HomeScreen';
-import Footer from './components/Footer';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+import AuthScreen from "./screens/AuthScreen";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <HomeScreen />
+      <Routes>
+        <Route path="/" element = {<HomeScreen />} />
+        <Route path="/auth" element = {<AuthScreen />} />
+      </Routes>
       <Footer />
     </div>
   );
---
+++ src/screens/AuthScreen.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AuthScreen = () => {
+    return (
+        <p>AUTHSCREEN</p>
+    );
+};
+
+export default AuthScreen;
